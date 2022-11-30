df37ec9f5ddc525c69edbd634c7a2699a7eb7293
gimmicks-u <is@pusan.ac.kr>
2022-12-01 02:13:11 +0900

    dist삭제, gitignore변경

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var app_1 = require('./src/app');
-app_1.server.listen();
--- dist/src/utils/dateFormatter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.dateToUSADate = void 0;
-var dateToUSADate = function (date) {
-  var formatter = new Intl.DateTimeFormat('en-US');
-  return formatter.format(date);
-};
-exports.dateToUSADate = dateToUSADate;
